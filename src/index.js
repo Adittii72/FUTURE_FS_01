86760403782ad14e1fff375159a7adf4249204d2
Adittii72 <aditi1411ss@gmail.com>
2025-11-07 16:21:58 +0530

    Set up Express server and connected backend with MySQL using Sequelize

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import sequelize from "./config/database.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("MySQL Database connected successfully!");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
